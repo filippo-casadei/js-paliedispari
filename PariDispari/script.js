900adb163a32ae6a31485ef444d6f8e1fde44cd6
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-31 18:12:41 +0200

    inserito  il mio ragionamento per lo svolgimento dell'esercizio.

--- PariDispari/script.js
+++ PariDispari/script.js
@@ -6,3 +6,16 @@
 // Dichiariamo chi ha vinto.
 
 // ======================================================================================
+
+// SCHEMA MENTALE: sicuramente dovrò inserire due prompt, uno per scegliere pari o dispari
+//  e uno in cui inserire un numero da 1 a 5. 
+// creo una funzione che chiamerò: function numeroPc () { } 
+// all'interno della funzione inserirò i comandi math.floor e math.random in modo che mi
+// generi un numero random intero da 1 a 5. 
+// Chiuderò la funzione con un return che sarà uguale al numero ottenuto generato 
+// randomicamente. Fuori dalla funzione creerò un'altra variabile che sarà il risultato della
+//  somma fra il numero scelto dall'utente e quello generato per il pc.
+// Con un altra funzione chiederò se questo numero sarà % 2 e in quel caso avrò come risultato
+//  0 o 1 e quindi potrò inserire if 0 pari else dispari.
+// dovrò poi confrontare la scelta dell'utente con il risultato ottenuto e dichiarare il
+// vincitore in console.log
