9c57473b48f766803770a5cebfa4ab480e9cd992
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-31 15:19:06 +0200

    inserito nel file script.js il mio schema mentale da segurie per la costruzione dell'esercizio.

---
+++ script.js
@@ -0,0 +1,11 @@
+// OBBIETTIVO: capire se una parola è palindroma, ossia che si legge uguale anche al
+// contrario.
+
+// ===================================================================================
+
+// STEPS:
+// 1- Chiedere all'utente una parola
+// 2- Creare una funzione che: - prenda una parola come input
+//                             - capisca se al contrario è uguale
+// 3 - se sono uguali --> è palindroma
+// 4- Se sono diverse --> non è palindroma
